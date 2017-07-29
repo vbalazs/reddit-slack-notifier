841490aecca64c1bd09b4f76f5f758e4a480d0e6
Balazs Varga <varbalazs@gmail.com>
2017-07-29 20:52:48 +0200

    Add Slack::Notifier

---
+++ lib/clients/slack/notifier.rb
@@ -0,0 +1,41 @@
+require "net/http"
+
+module Clients
+  module Slack
+    class Notifier
+      attr_reader :url, :channel_selector
+
+      def initialize(url:, channel_selector:)
+        @url = URI(url)
+        @channel_selector = channel_selector
+      end
+
+      def post(message)
+        channels = channel_selector.channels(message.title)
+
+        channels.each do |channel|
+          body = {
+            "channel" => channel,
+            "attachments" => [message.as_attachment]
+          }.to_json
+
+          notify(body)
+        end
+      end
+
+      private
+
+      def notify(body)
+        response = Net::HTTP.post(url, body, "Content-Type" => "application/json")
+
+        case response
+        when Net::HTTPSuccess then
+          response.body == "ok"
+        else
+          LOGGER.error("Failed to post to Slack: #{response.class} ; #{response.body}")
+          false
+        end
+      end
+    end
+  end
+end
---
+++ test/lib/clients/slack/notifier_test.rb
@@ -0,0 +1,59 @@
+require "test_helper"
+
+module Clients
+  module Slack
+    class NotifierTest < Minitest::Test
+      def test_posts_to_multiple_channels
+        stub_request(:any, /example/)
+
+        channel_selector = build_channel_selector
+        notifier = build_object(channel_selector)
+        channels_list = %w[ruby java]
+
+        channel_selector.stub :channels, channels_list do
+          notifier.post(build_message)
+
+          channels_list.each do |tag|
+            assert_requested(:post, "https://www.example.com/hooks/abc") do |req|
+              JSON.parse(req.body)["channel"] == tag
+            end
+          end
+        end
+      end
+
+      # rubocop:disable Metrics/AbcSize
+      def test_has_message_attachment
+        stub_request(:any, /example/)
+
+        channel_selector = build_channel_selector
+        notifier = build_object(channel_selector)
+
+        channel_selector.stub :channels, ["ruby"] do
+          notifier.post(build_message)
+
+          assert_requested(:post, "https://www.example.com/hooks/abc") do |req|
+            attachments = JSON.parse(req.body)["attachments"]
+
+            attachments.size == 1 &&
+              attachments[0]["image_url"] == "http://images.com/x.jpg"
+          end
+        end
+      end
+      # rubocop:enable Metrics/AbcSize
+
+      private
+
+      def build_channel_selector
+        ChannelSelector.new(default: "", whitelist: [])
+      end
+
+      def build_message
+        Message.new("image_url" => "http://images.com/x.jpg")
+      end
+
+      def build_object(channel_selector)
+        Notifier.new(url: "https://www.example.com/hooks/abc", channel_selector: channel_selector)
+      end
+    end
+  end
+end
