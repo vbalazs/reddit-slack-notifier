2712dbb9e00b6b54543d48d4ed142179bfc43468
Balazs Varga <varbalazs@gmail.com>
2017-07-30 18:56:32 +0200

    Add mocha gem for testing

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ end
 
 group :test do
   gem "minitest"
+  gem "mocha"
   gem "pry-byebug"
   gem "rubocop"
   gem "webmock", "~> 3.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 require_relative "../load"
 require "minitest/autorun"
 require "webmock/minitest"
+require "mocha/mini_test"
 
 LOGGER.level = "unknown" # mute it
