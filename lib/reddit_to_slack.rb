8a83df6af8c684265741ff2e18c2d6d1a4268340
Balazs Varga <varbalazs@gmail.com>
2017-08-06 01:06:37 +0200

    Add RedditToSlack which does the magic
    It queries reddit posts between the given time range and sends the notifications to Slack about them

---
+++ lib/reddit_to_slack.rb
@@ -0,0 +1,33 @@
+class RedditToSlack
+  attr_reader :slack_client, :reddit_query_object
+
+  def initialize(slack_client:, reddit_query_object:)
+    @slack_client = slack_client
+    @reddit_query_object = reddit_query_object
+  end
+
+  def call(timestamp_range)
+    posts = reddit_query_object.between(timestamp_range)
+
+    messages = posts.map { |post| build_message(post) }
+
+    messages.each do |msg|
+      slack_client.post(msg)
+    end
+  end
+
+  private
+
+  def build_message(post)
+    Clients::Slack::Message.new(
+      "title" => post.title_without_tags,
+      "tags" => post.tags,
+      "author" => post.author,
+      "post_url" => post.url,
+      "permalink" => post.permalink,
+      "image_url" => post.thumbnail,
+      "subreddit" => post.subreddit,
+      "posted_at" => post.posted_at
+    )
+  end
+end
---
+++ test/lib/reddit_to_slack.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class SchedulerTest < Minitest::Test
+  include ReddFactories
+
+  def test_sends_posts_to_slack
+    slack_client = Minitest::Mock.new
+    2.times { slack_client.expect(:post, true, [Clients::Slack::Message]) }
+    posts = [build_post, build_post]
+
+    reddit_query_object = Clients::Reddit::NewPostsQuery.new
+    object = build_object(slack_client, reddit_query_object)
+    r
+    reddit_query_object.stub :between, posts do
+      object.call(0)
+    end
+
+    slack_client.verify
+  end
+
+  private
+
+  def build_object(slack_client, reddit_query_object)
+    RedditToSlack.new(
+      slack_client: slack_client,
+      reddit_query_object: reddit_query_object
+    )
+  end
+
+  def build_post
+    Clients::Reddit::Post.new(build_redd_submission)
+  end
+end
